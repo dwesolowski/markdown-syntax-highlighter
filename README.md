7234b0f289964f44f20041ef95a4ee6d091b47ff
Daren Wesolowski <daren.wesolowski@icloud.com>
2018-07-22 18:16:00 -0400

    Initial import

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-markdown-syntax-highlighter
+# Markdown Syntax Highlighter for WordPress
+
+Adds easy syntax highlighting for Markdown in WordPress with [Prism](https://prismjs.com/)
+
+## Requirements
+
+This is not intended to be a standalone plugin, you will also need the following plugins.
+
+[Markdown Post Parser](https://github.com/dwesolowski/markdown-post-parser)  
+[Markdown Text Buttons](https://github.com/dwesolowski/markdown-text-buttons)
+
+## Prism Build
+
+* Lang - markup
+* Lang - css
+* Lang - clike
+* Lang - javascript
+* Lang - bash
+* Lang - markup emplating
+* Lang - markdown
+* Lang - php
+* Lang - php xtras
+* Plugin - command-line
+
+## Features
+
+Please see [Markdown syntax](https://www.markdownguide.org/cheat-sheet/) for reference.
+
+* For inline highlighting surround  \`text to highlight\` with back-ticks
+
+##### Result:
+
+Inline `highlighting section` example
+
+* For block highlighting use three \``` back-ticks followed by your language
+
+##### Example:
+
+<pre>
+```javascript
+var s = "JavaScript syntax highlighting";
+alert(s);
+```
+</pre>
+
+* For command prompt block highlighting as root use three \``` back-ticks followed by bash-root
+
+##### Example:
+
+<pre>
+```bash-root
+sudo apt update
+```
+</pre>
+
+* For command prompt block highlighting as user use three \``` back-ticks followed by bash-user
+
+##### Example:
+
+<pre>
+```bash-user
+sudo apt update
+```
+</pre>
+
+## Sample CSS to add to your theme CSS for inline code
+
+```css
+<style>
+    :not(pre) > code {
+        padding: .1em .4em .1em .4em;
+        border: 1px solid #d4cfcf;
+        border-radius: .3em;
+        white-space: normal;
+        color:#DD4A68;
+        background: #eee;
+        text-shadow: 0 1px white;
+    }
+    :not(pre) > code[class*="language-"],
+    pre[class*="language-"] {
+        background: #eee;
+    }
+</style>
+```
+
+## License
+
+Markdown Syntax Highlighter is licensed under the GPLv3.
---
+++ markdown-syntax-highlighter.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Plugin Name: Markdown Syntax Highlighter
+ * Plugin URI:
+ * Description: Adds easy syntax highlighting for Markdown in WordPress with Prism
+ * Version: 1.0
+ * Author: Daren Wesolowski
+ * Author URI:
+ * License:     GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Copyright (C) 2018  Daren Wesolowski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // exit if accessed directly!
+}
+
+define( 'PLUGINVERSION', '1.0.0' );
+define( 'PRISMJSVERSION', '1.15.0' );
+
+class MarkdownSyntaxHighlighter {
+
+    public function __construct() {
+
+        add_action( 'init', array( $this,'register_assets' ) );
+        add_filter( 'the_content', array( $this,'prism_content_filter' ), 99 );
+        add_filter( 'comment_text', array( $this,'prism_content_filter' ), 99 );
+    }
+
+    public function register_assets() {
+
+        wp_register_style( 'prismCSS', plugins_url( '/assets/prism/css/prism.css', __FILE__ ), array(), null );
+        wp_register_script( 'prismJS', plugins_url( '/assets/prism/js/prism.js', __FILE__ ), array(), PRISMJSVERSION, true );
+    }
+
+    public function prism_content_filter( $content ) {
+
+        $search = '@<pre><code(?: class="([\w-]+)")?>@';
+        $count = 0;
+
+        $content = preg_replace_callback( $search, function ( $matches ) {
+            if ( isset( $matches[1] ) ) {
+                if ( $matches[1] == 'language-bash-root' ) {
+                    return '<pre class="command-line language-bash" data-prompt="#" data-user="" data-host=""><code class="language-bash">';
+
+                } elseif ( $matches[1] == 'language-bash-user' ) {
+                    return '<pre class="command-line language-bash" data-prompt="$" data-user="" data-host=""><code class="language-bash">';
+
+                } else {
+                    return sprintf( '<pre><code class="%s">', $matches[1] );
+                }
+            } else {
+                return '<pre><code class="language-none">';
+            }
+        }, $content, -1, $count );
+
+        if ( $count ) {
+            wp_enqueue_style( 'prismCSS' );
+            wp_enqueue_script( 'prismJS' );
+        }
+        return $content;
+    }
+}
+
+$markdown_syntax_highlighter = new MarkdownSyntaxHighlighter();
